9de1e688c8fe61d558fcfcb17b4b209129dc1b31
Polarbear08 <pistolstar0808@gmail.com>
2019-12-15 16:16:09 +0900

    updated router

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.user('/macro', macroRouter);
+app.use('/macro', macroRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
